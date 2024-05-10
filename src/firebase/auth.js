38375f19a111b319ad45d54bea261d0ac04f567c
isaac opini <isaacopini8@gmail.com>
2024-05-10 11:04:31 +0300

    created google login functions

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,16 +1,39 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth } from "./config";
+import { GoogleAuthProvider } from "firebase/auth/cordova";
 
-const useSignInWithEmailAndPassword = async (email,password) => {
+const useSignInWithEmailAndPassword = async (email, password) => {
     try {
-      const res = await signInWithEmailAndPassword(email, password);
-      console.log("Signed in User ", { res });
-      
-   
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      console.log("Signed in User ", res);
+      return res;
     } catch (e) {
       console.error(e);
+      throw e; 
     }
   };
-
-  export default useSignInWithEmailAndPassword;
-
-// the below code fragment can be found in:
+  
+  const useCreateUserWithEmailAndPassword = async (email, password) => {
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log("User signed up: ", res);
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw e; 
+    }
+  };
+  
+  const useSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    try {
+      const res = await signInWithPopup(auth, provider);
+      console.log("Signed in with Google: ", res);
+      return res;
+    } catch (e) {
+      console.error(e);
+      throw e; 
+    }
+  };
+  
+  export { useSignInWithEmailAndPassword, useCreateUserWithEmailAndPassword, useSignInWithGoogle };
