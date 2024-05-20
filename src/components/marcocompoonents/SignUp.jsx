1694ad8db7c27f3cb281d0f38f51789f65fde9bb
isaac opini <isaacopini8@gmail.com>
2024-05-20 18:44:16 +0300

    added sign Up component

--- src/components/marcocompoonents/SignUp.jsx
+++ src/components/marcocompoonents/SignUp.jsx
@@ -6,12 +6,12 @@ export default function Signin(){
     return(
         <>
          <div className={`${styles}`}>
-            <HeaderLogo text='Welcome to Neonates, sign-In' head='Neonates'/>
+            <HeaderLogo text='Welcome to Neonates, sign-up' head='Neonates'/>
             <form action="">
             <Input type='text' ids='email' for='email' label='Email address' name='email' placeholder='Enter email address'  classes='bg-white' />
             <Input type='password' ids='Password' for='password' label='Password' name='password' placeholder='Enter password'  classes='bg-white' />
            <div className={`${buttonStyle}`}>
-            <RoundedButton text='Sign-in' link= '/User/choose_user_role' />
+            <RoundedButton text='Sign-Up' link= '/User/choose_user_role' />
             <Alt endText='Forgot password' />
              <Alt highlightText='Sign-up' endText='instead'link= '/User/choose_user_role' />
             
