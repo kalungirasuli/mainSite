afdae6863e6caf4048c3486fb3fcf04c7ed4ec32
isaac opini <isaacopini8@gmail.com>
2024-05-27 23:12:08 +0300

    added the list of all doctors

--- src/components/marcocompoonents/DoctorAll.jsx
+++ src/components/marcocompoonents/DoctorAll.jsx
@@ -1,22 +1,63 @@
-import DoctorListcard from "../microcomponents/DoctorListcard"
-import HeadWithBack from "../microcomponents/HeadWithBack"
-import { Search } from "../microcomponents/textComponents"
+import  { useEffect, useState } from "react";
 
-export default function DoctorAll(){
-    return(
-        <>
+import DoctorListcard from "../microcomponents/DoctorListcard";
+import HeadWithBack from "../microcomponents/HeadWithBack";
+import { Search } from "../microcomponents/textComponents";
+import { getAllDoctors } from "../../firebase/doctors";
+
+export default function DoctorAll() {
+  const [doctors, setDoctors] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const doctorsList = await getAllDoctors();
+        console.log(doctorsList)
+        setDoctors(doctorsList);
+        console.log(doctors)
+      } catch (error) {
+        console.error("Error fetching doctors:", error);
+      }
+    };
+
+    fetchDoctors();
+  }, []);
+
+  const filteredDoctors = doctors.filter(doctor =>
+    doctor.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="div">
+        <HeadWithBack heading="Pediatricians list" />
         <div className="div">
-            <HeadWithBack heading='Pedetriciations list'/>
-            <div className="div">
-            <div className="div p-3 h-auto">
-                <Search type='text' placeholder='Search a pedetriciation' ids='search' classes='rounded-[10px] p-3 w-full border-0 text-left text-[15px] outline-none'/>
-            </div>
-              <div className="div w-[90%] m-auto flex flex-wrap justify-between gap-[10px] pt-5">
-                {/* the props are, name, work, link, image, online is aboolean */}
-              <DoctorListcard/>
-              </div>
-            </div>  
+          <div className="div p-3 h-auto">
+            <Search
+              type="text"
+              placeholder="Search a pediatrician"
+              ids="search"
+              classes="rounded-[10px] p-3 w-full border-0 text-left text-[15px] outline-none"
+              value={searchTerm}
+              onChange={e => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <div className="div w-[90%] m-auto flex flex-wrap justify-between gap-[10px] pt-5">
+            {filteredDoctors.map(doctor => (
+              <DoctorListcard
+                key={doctor.id}
+                name={doctor.firstName + ' ' + doctor.secondName
+                }
+                work={doctor.work}
+                link={`/doctor/${doctor.id}`}
+                image={doctor.image}
+                online={doctor.online}
+              />
+            ))}
+          </div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  );
 }
