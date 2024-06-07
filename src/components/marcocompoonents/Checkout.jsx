59cbfffac0923528f2333d09bc507da153711bab
isaacopini <isaacopini8@gmail.com>
2024-06-07 19:07:05 +0300

    re intergrated mobile money

--- src/components/marcocompoonents/Checkout.jsx
+++ src/components/marcocompoonents/Checkout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import HeadWithBack from "../microcomponents/HeadWithBack";
 import { Input } from "../microcomponents/textComponents";
 import { Button3 } from "../microcomponents/RoundedButton";
+import axios from 'axios';
 
 export default function Checkout() {
     const selectedDoctor = useSelector(state => state.user.selectedDoctor);
     const bookingDetails = useSelector(state => state.user.bookingDetails);
+
+    const [phoneNumber, setPhoneNumber] = useState('');
 
     const data = {
         doctor: `${selectedDoctor.firstName} ${selectedDoctor.secondName}`,
@@ -15,8 +18,25 @@ export default function Checkout() {
         date: bookingDetails.day,
     };
 
-    const handleClick = () => {
-        window.location.href = 'http://localhost:3001';
+    const handlePhoneNumberChange = (e) => {
+        setPhoneNumber(e.target.value);
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        const paymentDetails = {
+            total: 100, // Example total amount
+            phone: phoneNumber,
+        };
+
+        try {
+            const response = await axios.post('http://localhost:3000/pay', paymentDetails);
+            console.log('Payment Response:', response.data);
+            window.location.href = 'http://localhost:3000';
+        } catch (error) {
+            console.error('Error making payment request:', error);
+        }
     };
 
     return (
@@ -31,16 +51,15 @@ export default function Checkout() {
                     <p className="flex flex-row text-greytextdark text-[25px] p-4 justify-evenly"><span className="font-bold text-greytextdark text-left">Time</span>:<span>{data.time}</span></p>
                     <p className="flex flex-row text-greytextdark text-[20px] p-4 justify-evenly"><span className="font-bold text-greytextdark text-left">Date</span>:<span>{data.date}</span></p>
                 </div>
-                <form className="w-[90%] m-auto mt-[50px] pt-[100px]">
+                <form className="w-[90%] m-auto mt-[50px] pt-[100px]" onSubmit={handleClick}>
                     <div className="div pt-[50px]">
-                        <Input label='MTN Phone number' placeholder='Enter phone number (MTN)' />
+                        <Input label='MTN Phone number' placeholder='Enter phone number (MTN)' value={phoneNumber} onChange={handlePhoneNumberChange} />
                     </div>
                     <div className="div mt-5 w-full">
-                        <Button3 text='Check out' bg='bg-blue' color='text-white' rounded='rounded-[10px]' width='w-[90%] m-auto' onClick={handleClick} />
+                        <Button3 text='Check out' bg='bg-blue' color='text-white' rounded='rounded-[10px]' width='w-[90%] m-auto'  onClick = {handleClick}/>
                     </div>
                 </form>
             </div>
         </>
     );
 }
-
