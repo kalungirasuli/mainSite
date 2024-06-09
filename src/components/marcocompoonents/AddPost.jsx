79ada7076db3e2172619194e35377d8c8d53cdfd
isaacopini <isaacopini8@gmail.com>
2024-06-09 12:41:11 +0300

    intergrated profile update

--- src/components/marcocompoonents/AddPost.jsx
+++ src/components/marcocompoonents/AddPost.jsx
@@ -1,15 +1,61 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeadWithBack from "../microcomponents/HeadWithBack";
 import { TextArea, FilePicker } from "../microcomponents/textComponents";
 import { createPost } from "../../firebase/post";
 import RoundedButton from "../microcomponents/RoundedButton";
 import { useSelector } from "react-redux";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../firebase/config";
+import { useNavigate } from "react-router-dom";
 
 export default function AddPost() {
   const userUid = useSelector((state) => state.auth.user);
   const [content, setContent] = useState("");
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const user = useSelector(state => state.auth.user);
+  const [userType, setUserType] = useState(null);
+  const navigate = useNavigate()
+  useEffect(() => {
+    
+  
+    // Function to determine user type
+    const determineUserType = async () => {
+      if (user) {
+        try {
+          console.log("User ID: ", user);
+          const uid = user;
 
+          // Check if the user is a doctor
+          const doctorQuery = query(collection(db, 'doctors'), where('uid', '==', uid));
+          const doctorSnapshot = await getDocs(doctorQuery);
+
+          if (!doctorSnapshot.empty) {
+            setUserType('doctor');
+            console.log("User is a doctor");
+            return;
+          }
+
+          // Check if the user is a mother
+          const motherQuery = query(collection(db, 'mothers'), where('uid', '==', uid));
+          const motherSnapshot = await getDocs(motherQuery);
+
+          if (!motherSnapshot.empty) {
+            setUserType('mother');
+            console.log("User is a mother");
+          }
+        } catch (error) {
+          console.error("Error determining user type: ", error);
+        }
+      } else {
+        navigate('/User/sign-in')
+        console.log("No user found");
+      }
+    };
+
+    determineUserType();
+
+
+  }, [user]);
   const handleFileChange = (files) => {
     setSelectedFiles(files);
   };
