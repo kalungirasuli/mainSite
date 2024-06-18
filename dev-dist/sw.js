35d77141e15e8b2308109f583d972db05d6ed0cc
kalungi rasuli <kalungirasuli495@gmail.com>
2024-06-18 18:18:07 +0300

    dist changed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a93h2h5r2ig"
+    "revision": "0.g9civ33qm2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
